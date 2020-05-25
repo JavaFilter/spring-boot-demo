d957bb2fe77854664bb5bac2c00535bcf7c9b7f9
JavaFilter <1106058183@qq.com>
2020-05-25 16:38:13 +0800

    初次提交

---
+++ spring-boot-demo-redis/src/main/java/com/shenyu/redis/SpringbootDemoRedisApplication.java
@@ -0,0 +1,13 @@
+package com.shenyu.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoRedisApplication.class, args);
+    }
+
+}
---
+++ spring-boot-demo-redis/src/main/java/com/shenyu/redis/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.shenyu.redis.controller;
+
+/**
+ * @ClassName controller
+ * @Author shenyu
+ * @Date 2020/5/25
+ * @Version 1.0.0
+ */
+public class UserController {
+}
