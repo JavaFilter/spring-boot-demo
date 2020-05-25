096febbc286ebeed9436166966f4a7df4a368f10
shenyu <haibing.mei@visionacademy.cn>
2020-05-25 16:38:13 +0800

    初次提交

---
+++ spring-boot-demo-redis/src/test/java/com/shenyu/redis/SpringbootDemoRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shenyu.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
