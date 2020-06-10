dedb759c2100553375c30486df6d5c0d01d3f01b
JavaFilter <1106058183@qq.com>
2020-06-10 16:21:53 +0800

    添加自定义异常

--- spring-boot-exception/src/main/java/com/shenyu/exception/controller/DemoExceptionController.java
+++ spring-boot-exception/src/main/java/com/shenyu/exception/controller/DemoExceptionController.java
@@ -1,5 +1,6 @@
 package com.shenyu.exception.controller;
 
+import com.shenyu.exception.BusinessException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,10 +18,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/demo")
 public class DemoExceptionController {
 
+    /**
+     * 全局异常
+     * @return
+     */
     @PostMapping("/getException")
     public String getException(){
         if (1==1){
             throw new RuntimeException("这是一个RuntimeException!");
+        }
+        return "success";
+    }
+
+    /**
+     * 自定义异常
+     * @return
+     */
+    @PostMapping("/getBusinessException")
+    public String getBusinessException(){
+        if (1==1){
+            throw new BusinessException(500,"这是一个自定义Exception!");
         }
         return "success";
     }
--- spring-boot-exception/src/main/java/com/shenyu/exception/handler/GlobalExceptionHandler.java
+++ spring-boot-exception/src/main/java/com/shenyu/exception/handler/GlobalExceptionHandler.java
@@ -4,31 +4,10 @@ import com.shenyu.exception.BusinessException;
 import com.shenyu.exception.CommonResponseEnum;
 import com.shenyu.exception.http.response.BaseErrorResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.ConversionNotSupportedException;
-import org.springframework.beans.TypeMismatchException;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.stereotype.Component;
-import org.springframework.validation.BindException;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
-import org.springframework.web.HttpMediaTypeNotAcceptableException;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingPathVariableException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
-import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
-import org.springframework.web.multipart.support.MissingServletRequestPartException;
-import org.springframework.web.servlet.NoHandlerFoundException;
 
 /**
  * @desc 全局异常处理器
