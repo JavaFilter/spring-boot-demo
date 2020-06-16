44cbd4d5236018085dc98692573ba98f6c8524fa
JavaFilter <1106058183@qq.com>
2020-06-16 14:05:39 +0800

    添加异常事咧

--- spring-boot-exception/src/main/java/com/shenyu/exception/controller/ExceptionController.java
+++ spring-boot-exception/src/main/java/com/shenyu/exception/controller/ExceptionController.java
@@ -1,10 +1,27 @@
 package com.shenyu.exception.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.shenyu.exception.assertion.CommonExceptionEnum.ARGUMENT_VALID_ERROR;
+
+
 /**
  * @ClassName ExceptionController
  * @Author shenyu
  * @Date 2020/6/9
  * @Version 1.0.0
  */
+@RestController
+@RequestMapping("/assert")
 public class ExceptionController {
+
+
+    @GetMapping("/getException")
+    public String getException(){
+        ARGUMENT_VALID_ERROR.assertIsNull(null);
+        return "ok";
+    }
+
 }
