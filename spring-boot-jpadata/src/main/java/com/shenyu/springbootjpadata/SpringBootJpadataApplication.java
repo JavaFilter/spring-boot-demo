4c67a8da7e6b07e40c219890f823620411709723
JavaFilter <1106058183@qq.com>
2020-06-17 11:10:45 +0800

    添加spring-boot 集成spring-data-jpa模块

---
+++ spring-boot-jpadata/src/main/java/com/shenyu/springbootjpadata/SpringBootJpadataApplication.java
@@ -0,0 +1,13 @@
+package com.shenyu.springbootjpadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpadataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpadataApplication.class, args);
+    }
+
+}
