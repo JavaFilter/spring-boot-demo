de8cf7b929f9bd3c742bc92af2b54a2dd18a8fc7
JavaFilter <1106058183@qq.com>
2020-07-14 15:20:55 +0800

    添加需要登陆访问的接口

--- spring-boot-security/src/main/java/com/shenyu/springbootsecurity/controller/UserController.java
+++ spring-boot-security/src/main/java/com/shenyu/springbootsecurity/controller/UserController.java
@@ -1,5 +1,9 @@
 package com.shenyu.springbootsecurity.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,8 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @Version 1.0.0
  */
 @RestController
+@RequestMapping("/demo")
 public class UserController {
 
-
+    @PreAuthorize("hasAuthority('sys:user:view')")
+    @GetMapping(value="/findAll")
+    public String findAll() {
+        return "ok";
+    }
 
 }
